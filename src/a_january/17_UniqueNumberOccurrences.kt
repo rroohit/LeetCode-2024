643971f6f15b81b2bcc378576df7f0dfc7be4ba9
rohitt <crohitp9@gmail.com>
2024-01-17 23:13:43 +0530

    January-Problems-Solved.

---
+++ src/a_january/17_UniqueNumberOccurrences.kt
@@ -0,0 +1,43 @@
+package a_january
+
+/**
+ *  Problem = 17 : Unique Number of Occurrences
+ *
+ *  # Intuition  :
+ *
+ *
+ *  # Approach   :
+ *
+ *
+ *  # Complexity :
+ *
+ *      - Time complexity: (n)
+ *
+ *      - Space complexity: (m)
+ *
+ * # Code =>
+ */
+fun main() {
+
+    val arr = intArrayOf(1, 2, 2, 1, 1, 3)
+
+    val result = uniqueOccurrences(arr)
+
+    println("Result ==> $result")
+
+}
+
+fun uniqueOccurrences(arr: IntArray): Boolean {
+    // map : num -> count
+    val map = hashMapOf<Int, Int>()
+
+    arr.forEach {
+        val currCount = map.getOrDefault(it, 0)
+        map[it] = currCount + 1
+    }
+
+    val set = mutableSetOf<Int>()
+    map.forEach { set.add(it.value) }
+
+    return set.size == map.size
+}
