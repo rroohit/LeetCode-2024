2a8670ddffede85a0920ae02f8486298d10b18f6
rohitt <crohitp9@gmail.com>
2024-01-26 12:14:32 +0530

    January-Problems-Solved.

---
+++ src/a_january/26_OutBoundaryPaths.kt
@@ -0,0 +1,58 @@
+package a_january
+
+/**
+ *  Problem = 26 : Out of Boundary Paths
+ *
+ *
+ *  # Complexity :
+ *
+ *      - Time complexity: (n + m)
+ *
+ *      - Space complexity: (n)
+ *
+ * # Code =>
+ */
+fun main() {
+
+    val m = 2
+    val n = 2
+    val maxMove = 2
+    val startRow = 0
+    val startColumn = 0
+
+    val result = findPaths(m, n, maxMove, startRow, startColumn)
+
+    println("Result ==> $result")
+
+}
+
+
+fun findPaths(m: Int, n: Int, maxMove: Int, startRow: Int, startColumn: Int): Int {
+    return dfs(m, n, startRow, startColumn, maxMove).toInt()
+}
+
+val dp_cache = mutableMapOf<Triple<Int, Int, Int>, Long>()
+
+fun dfs(m: Int, n: Int, r: Int, c: Int, mov: Int): Long {
+    val mod = 1000000007
+
+    if (r < 0 || r == m || c < 0 || c == n) {
+        return 1
+    }
+
+    if (mov == 0) {
+        return 0
+    }
+
+    val key = Triple(r, c, mov)
+    if (key in dp_cache) {
+        return dp_cache[key]!!
+    }
+
+    val result = ((dfs(m, n, r + 1, c, mov - 1) + dfs(m, n, r - 1, c, mov - 1)) % mod +
+                    (dfs(m, n, r, c + 1, mov - 1) + dfs(m, n, r, c - 1, mov - 1)) + mod
+            ) % mod
+
+    dp_cache[key] = result
+    return result
+}
