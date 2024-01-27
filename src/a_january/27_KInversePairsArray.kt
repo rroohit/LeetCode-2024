2996937ddb612357231fb95fcfb79199de4fa8a9
rohitt <crohitp9@gmail.com>
2024-01-27 11:42:27 +0530

    January-Problems-Solved.

---
+++ src/a_january/27_KInversePairsArray.kt
@@ -0,0 +1,32 @@
+package a_january
+
+// K-Inverse Pairs Array
+fun main() {
+
+    val n = 3
+    val k = 1
+
+    println("Result ==> ${kInversePairs(n, k)}")
+
+}
+
+
+fun kInversePairs(n: Int, k: Int): Int {
+    val mod = 1000000007
+    var prev = IntArray(k + 1) { 0 }
+    prev[0] = 1
+
+    for (N in 1..n ) {
+        val curr = IntArray(k + 1) { 0 }
+        var total = 0L
+        for (K in 0..k ) {
+            if (K >= N) {
+                total -= prev[K - N]
+            }
+            total = (total + prev[K]) % mod
+            curr[K] = total.toInt()
+        }
+        prev = curr
+    }
+    return prev[k]
+}
