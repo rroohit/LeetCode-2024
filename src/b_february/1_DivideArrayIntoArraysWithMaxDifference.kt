63331357854a845d948658a3eba1503e7bf5c607
rohitt <crohitp9@gmail.com>
2024-02-01 21:40:57 +0530

    January-Problems-Solved.

---
+++ src/b_february/1_DivideArrayIntoArraysWithMaxDifference.kt
@@ -0,0 +1,46 @@
+package b_february
+
+/**
+ *  Problem = 1 : Divide Array Into Arrays With Max Difference
+ *
+ *
+ *  # Complexity :
+ *
+ *      - Time complexity: (n)
+ *
+ *      - Space complexity: (n)
+ *
+ * # Code =>
+ */
+fun main() {
+
+    val nums = intArrayOf(1, 3, 4, 8, 7, 9, 3, 5, 1)
+    val k = 2
+
+    val result = divideArray(nums, k)
+
+    result.forEach {
+        println(it.toList())
+    }
+
+}
+
+fun divideArray(nums: IntArray, k: Int): Array<IntArray> {
+    val ans = mutableListOf<IntArray>()
+
+    nums.sort()
+
+    for (i in 2 ..< nums.size step 3) {
+        if (nums[i] - nums[i - 2] <= k) {
+            val arr = IntArray(3)
+            for (j in i downTo i - 2) {
+                arr[j % 3] = nums[j]
+            }
+            ans.add(arr)
+        } else {
+            return arrayOf()
+        }
+    }
+
+    return ans.toTypedArray()
+}
