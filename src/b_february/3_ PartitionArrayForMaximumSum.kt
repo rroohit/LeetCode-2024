b9af39aec165ea759ce441b14c74d7b3bb3aba69
rohitt <crohitp9@gmail.com>
2024-02-03 13:13:43 +0530

    February-Problems-Solved.

--- src/b_february/3_ PartitionArrayForMaximumSum.kt
+++ src/b_february/3_ PartitionArrayForMaximumSum.kt
@@ -1,5 +1,8 @@
 package b_february
 
+import kotlin.math.max
+import kotlin.math.min
+
 /**
  *  Problem = 3 : Partition Array for Maximum Sum
  *
@@ -12,3 +15,33 @@ package b_february
  *
  * # Code =>
  */
+fun main() {
+
+    val arr = intArrayOf(1, 15, 7, 9, 2, 5, 10)
+    val k = 3
+
+    val result = maxSumAfterPartitioning(arr, k)
+
+    println("Result ==> $result")
+
+}
+
+fun maxSumAfterPartitioning(arr: IntArray, k: Int): Int {
+    return dfs(arr, 0, k)
+}
+
+private val cache = mutableMapOf<Int, Int>()
+private fun dfs(arr: IntArray, i: Int, k: Int): Int {
+    if (i >= arr.size) return 0
+    if (cache.containsKey(i)) return cache[i]!!
+
+    var res = 0
+    var max = 0
+    for (j in i..<min(i + k, arr.size)) {
+        max = max(max, arr[j])
+        val windowSize = j - i + 1
+        res = max(res, dfs(arr, j + 1, k) + max * windowSize)
+    }
+    cache[i] = res
+    return res
+}
