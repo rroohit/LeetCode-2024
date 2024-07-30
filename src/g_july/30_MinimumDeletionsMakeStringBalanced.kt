cbc82a977b091705baaeb80e242ac39149b9ef7f
rohitt <crohitp9@gmail.com>
2024-07-30 09:44:18 +0530

    July-Problems-Solving. 30

---
+++ src/g_july/30_MinimumDeletionsMakeStringBalanced.kt
@@ -0,0 +1,47 @@
+package g_july
+
+import kotlin.math.min
+
+/**
+ *  Problem 30. Minimum Deletions to Make String Balanced
+ *
+ *  ## Intuition -
+ *
+ *  ## Approach -
+ *
+ *  ## Complexity:
+ *       - Time complexity: O(N)
+ *
+ *       - Space complexity: O(N)
+ *
+ * ## Code -
+ */
+fun main() {
+
+    val testCases = listOf(
+        "aababbab",
+        "bbaaaaabb"
+    )
+
+    testCases.forEach { str ->
+        println("Result ==> ${minimumDeletions(str)}")
+    }
+
+}
+
+fun minimumDeletions(s: String): Int {
+    val aCount = IntArray(s.length)
+    for (i in s.length - 2 downTo 0) {
+        aCount[i] = aCount[i + 1]
+        aCount[i] += if (s[i + 1] == 'a') 1 else 0
+    }
+
+    var bCount = 0
+    var totalDelete = s.length
+    for ((i,c) in s.withIndex()) {
+        totalDelete = min(totalDelete, aCount[i] + bCount)
+        if (c == 'b') bCount++
+    }
+
+    return totalDelete
+}
