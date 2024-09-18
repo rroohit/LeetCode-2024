7dfc16936aeb84917e4b7f5a1138766ca6983b58
rohitt <crohitp9@gmail.com>
2024-09-18 20:21:46 +0530

    September-Problems-Solving ->    18. Largest Number

--- src/i_september/18_LargestNumber.kt
+++ src/i_september/18_LargestNumber.kt
@@ -28,16 +28,16 @@ fun main() {
 }
 
 fun largestNumber(nums: IntArray): String {
-    val sorted = nums.sortedWith(Comparator { a, b ->
-        val a1 = a.toString();
-        val b1 = b.toString();
-        (b1 + a1).compareTo(a1 + b1);
-    })
-    if (sorted[0] == 0) return "0";
-
-    val ans = StringBuilder();
-    for (it in sorted) {
-        ans.append(it.toString());
+    val sorted = nums.sortedWith { a, b ->
+        val a1 = a.toString()
+        val b1 = b.toString()
+        (b1 + a1).compareTo(a1 + b1)
     }
-    return ans.toString();
+    if (sorted[0] == 0) return "0"
+
+    val ans = StringBuilder()
+    for (it in sorted) {
+        ans.append(it.toString())
+    }
+    return ans.toString()
 }
