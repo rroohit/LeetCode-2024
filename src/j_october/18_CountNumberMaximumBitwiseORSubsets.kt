8a9f25495e10b9d86c1b300737e110948cdbb94b
rohitt <crohitp9@gmail.com>
2024-10-18 15:17:12 +0530

    October-Problems-Solving ->     18. Count Number of Maximum Bitwise-OR Subsets

--- src/j_october/18_CountNumberMaximumBitwiseORSubsets.kt
+++ src/j_october/18_CountNumberMaximumBitwiseORSubsets.kt
@@ -30,17 +30,32 @@ fun main() {
 
 fun countMaxOrSubsets(nums: IntArray): Int {
     var maxOr = 0
-    for (num in nums) {
-        maxOr = maxOr or num
-    }
-    return countSubset(nums, 0, 0, maxOr)
+    for (num in nums) maxOr = maxOr or num
+    val memo = Array(nums.size) { arrayOfNulls<Int>(maxOr + 1) }
+    return countSubset(nums, 0, 0, maxOr, memo)
 }
 
-private fun countSubset(nums: IntArray, i: Int, currOr: Int, targetOr: Int): Int {
+// memoization + recursion :: TC - O(n * maxOr) :: SC - O(n * maxOr)
+private fun countSubset(nums: IntArray, i: Int, currOr: Int, targetOr: Int, memo: Array<Array<Int?>>): Int {
+    if (i == nums.size) {
+        return if (currOr == targetOr) 1 else 0
+    }
+
+    if (memo[i][currOr] != null) return memo[i][currOr]!!
+
+    memo[i][currOr] = countSubset(nums, i + 1, currOr, targetOr, memo) +
+            countSubset(nums, i + 1, currOr or nums[i], targetOr, memo)
+    return memo[i][currOr]!!
+}
+
+////////////////////////////////////
+// 1
+// recursion TC - O(2^n) :: SC - O(N)
+private fun countSubset1(nums: IntArray, i: Int, currOr: Int, targetOr: Int): Int {
     return if (i == nums.size) {
         if (currOr == targetOr) 1 else 0
     } else {
-        countSubset(nums, i + 1, currOr, targetOr) +
-                countSubset(nums, i + 1, currOr or nums[i], targetOr)
+        countSubset1(nums, i + 1, currOr, targetOr) +
+                countSubset1(nums, i + 1, currOr or nums[i], targetOr)
     }
 }
