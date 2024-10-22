e47b22722cfeea868a27d420a01c5c13394a338d
rohitt <crohitp9@gmail.com>
2024-10-22 13:29:27 +0530

    October-Problems-Solving ->     22. Kth Largest Sum In Binary Tree.

---
+++ src/j_october/22_KthLargestSumInBinaryTree.kt
@@ -0,0 +1,57 @@
+package j_october
+
+import z_data_types.TreeNode
+import z_data_types.createTree
+import java.util.*
+
+
+/**
+ *  Problem 22. Kth Largest Sum In Binary Tree.
+ *
+ *  ## Intuition -
+ *
+ *  ## Approach -
+ *
+ *  ## Complexity:
+ *       - Time complexity: O(log^2N + k*logN)
+ *
+ *       - Space complexity: O(N)
+ *
+ * ## Code -
+ */
+fun main() {
+
+    val testCases = listOf(
+        Pair(listOf(5, 8, 9, 2, 1, 3, 7, 4, 6), 2)
+    )
+
+    testCases.forEach { test ->
+        val root = createTree(test.first)
+        println("Result ==> ${kthLargestLevelSum(root, test.second)}")
+    }
+
+}
+
+fun kthLargestLevelSum(root: TreeNode?, k: Int): Long {
+    val pq = PriorityQueue(
+        Collections.reverseOrder<Long>()
+    )
+    val bfsQueue: Queue<TreeNode> = LinkedList()
+    bfsQueue.offer(root)
+    while (bfsQueue.isNotEmpty()) {
+        val currLevelSize = bfsQueue.size
+        var currSum = 0L
+        for (i in 0..<currLevelSize) {
+            val top = bfsQueue.poll()
+            currSum += top.`val`
+            if (top.left != null) bfsQueue.offer(top.left)
+            if (top.right != null) bfsQueue.offer(top.right)
+        }
+        pq.offer(currSum)
+    }
+    if (pq.size < k) return -1
+    repeat(k - 1) {
+        pq.poll()
+    }
+    return pq.peek()!!
+}
