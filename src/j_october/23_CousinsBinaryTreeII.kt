8653816e3799f1ec42ce4c30ebf5d064c3964ef7
rohitt <crohitp9@gmail.com>
2024-10-23 20:51:52 +0530

    October-Problems-Solving ->     23. Cousins in Binary Tree II.

--- src/j_october/23_CousinsBinaryTreeII.kt
+++ src/j_october/23_CousinsBinaryTreeII.kt
@@ -33,7 +33,41 @@ fun main() {
 
 }
 
+// DFS
+private val levelSums = IntArray(100000)
 fun replaceValueInTree(root: TreeNode?): TreeNode? {
+    calculateLevelSum(root, 0)
+    replaceValuesInNode(root, 0, 0)
+    return root
+}
+
+private fun replaceValuesInNode(
+    root: TreeNode?,
+    sibSum: Int, // sibling sum
+    level: Int
+) {
+    if (root == null) return
+    val leftVal = root.left?.`val` ?: 0
+    val rightVal = root.right?.`val` ?: 0
+    if (level <= 1) {
+        root.`val` = 0
+    } else {
+        root.`val` = levelSums[level] - root.`val` - sibSum
+    }
+    replaceValuesInNode(root.left, rightVal, level + 1)
+    replaceValuesInNode(root.right, leftVal, level + 1)
+}
+
+private fun calculateLevelSum(root: TreeNode?, level: Int) {
+    if (root == null) return
+    levelSums[level] += root.`val`
+    calculateLevelSum(root.left, level + 1)
+    calculateLevelSum(root.right, level + 1)
+}
+
+/////////////////////////////
+// BFS
+fun replaceValueInTree1(root: TreeNode?): TreeNode? {
     if (root == null) return null
     val levelSums = mutableListOf<Int>()
     val nodeQu: Queue<TreeNode?> = LinkedList()
