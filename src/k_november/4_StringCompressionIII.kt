df42ee4f13ed9d6aeca8aaef003931dc02777da6
rohitt <crohitp9@gmail.com>
2024-11-04 15:44:45 +0530

    November-Problems-Solving ->     4. String Compression III

---
+++ src/k_november/4_StringCompressionIII.kt
@@ -0,0 +1,46 @@
+package k_november
+
+/**
+ *  Problem 4. String Compression III
+ *
+ *  ## Intuition -
+ *
+ *  ## Approach -
+ *
+ *  ## Complexity:
+ *       - Time complexity: O(N)
+ *
+ *       - Space complexity: O(N)
+ *
+ * ## Code -
+ */
+fun main() {
+
+    val testCases = listOf(
+        "abcde",
+        "aaaaaaaaaaaaaabb",
+        "rrkkssoolllllllllllllllllllllllllllllllllllll"
+    )
+
+    testCases.forEach { word ->
+        println("Result ==> ${compressedString(word)}")
+    }
+
+}
+
+fun compressedString(word: String): String {
+    val sb = StringBuilder()
+    var i = 0
+    while (i < word.length) {
+        val curr = word[i]
+        var count = 1
+        while (i < word.length - 1 && count < 9 && curr == word[i + 1]) {
+            i++
+            count++
+        }
+        sb.append("$count$curr")
+        i++
+    }
+
+    return sb.toString()
+}
