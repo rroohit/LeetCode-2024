b42fdb3edd594617fce83a4706682cd34e073128
rohitt <crohitp9@gmail.com>
2024-12-02 10:36:32 +0530

    December-Problems-Solving ->     2. Check If a Word Occurs As a Prefix of Any Word in a Sentence.

--- src/l_december/2_CheckIfWordOccursAsPrefixOfAnyWordSentence.kt
+++ src/l_december/2_CheckIfWordOccursAsPrefixOfAnyWordSentence.kt
@@ -10,9 +10,9 @@ import kotlin.math.min
  *  ## Approach -
  *
  *  ## Complexity:
- *       - Time complexity: O(n * m)
+ *       - Time complexity: O(n)
  *
- *       - Space complexity: O(n)
+ *       - Space complexity: O(1)
  *
  * ## Code -
  */
@@ -29,8 +29,29 @@ fun main() {
 
 }
 
-// TC - O(n * m) :: SC - O(n)
 fun isPrefixOfWord(sentence: String, searchWord: String): Int {
+    var wordInd = 1
+    var l = 0
+
+    while (l < sentence.length) {
+        var prefInd = 0
+        while (prefInd < searchWord.length && l < sentence.length && searchWord[prefInd] == sentence[l]) {
+            prefInd++
+            l++
+        }
+
+        if (prefInd == searchWord.length) return wordInd
+        while (l < sentence.length && sentence[l] != ' ') l++
+
+        l++
+        wordInd++
+    }
+
+    return -1
+}
+
+// TC - O(n * m) :: SC - O(n)
+fun isPrefixOfWord1(sentence: String, searchWord: String): Int {
     val listOfWords = sentence.split(" ") // TC & SC - O(n)
     for (i in listOfWords.indices) { // TC - O(n)
         val word = listOfWords[i]
