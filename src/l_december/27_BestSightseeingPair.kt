0e12d86cd7af6658b27d67c60f24ede5fa6d164a
rohitt <crohitp9@gmail.com>
2024-12-27 17:16:04 +0530

    December-Problems-Solving ->     27. Best Sightseeing Pair.

--- src/l_december/27_BestSightseeingPair.kt
+++ src/l_december/27_BestSightseeingPair.kt
@@ -13,16 +13,17 @@ import kotlin.math.max
  *  ## Complexity:
  *       - Time complexity: O(n)
  *
- *       - Space complexity: O(n)
+ *       - Space complexity: O(1)
  *
  * ## Code -
  */
 fun main() {
 
     val testCases = listOf(
-        intArrayOf(8, 1, 5, 2, 6),
-        intArrayOf(7, 2, 6, 6, 9, 4, 3),
-        intArrayOf(1, 2)
+        intArrayOf(8, 1, 5, 2, 6), // 11
+        intArrayOf(7, 2, 6, 6, 9, 4, 3), // 14
+        intArrayOf(1, 2), // 2
+        intArrayOf(4, 7, 5, 8) //14
     )
 
     testCases.forEach { values ->
@@ -32,11 +33,27 @@ fun main() {
 
 }
 
+// Dynamic Programming - Space optimized
+// TC - O(n)
+// SC - (1)
 fun maxScoreSightseeingPair(values: IntArray): Int {
+    var maxScore = 0
+    var left = values.first()
+    for (j in 1..<values.size) {
+        val right = values[j]
+        maxScore = max(maxScore, left + right - j)
+        if (right + j >= left) left = right + j
+    }
+    return maxScore
+}
+
+// Dynamic Programming
+// TC - O(n)
+// SC - (n)
+fun maxScoreSightseeingPair1(values: IntArray): Int {
     val n: Int = values.size
     val maxLeftScore = IntArray(n)
     maxLeftScore[0] = values[0]
-
     var maxScore = 0
     for (i in 1..<n) {
         val currentRightScore = values[i] - i
@@ -44,10 +61,8 @@ fun maxScoreSightseeingPair(values: IntArray): Int {
             maxScore,
             (maxLeftScore[i - 1] + currentRightScore)
         )
-
         val currentLeftScore = values[i] + i
         maxLeftScore[i] = max(maxLeftScore[i - 1], currentLeftScore)
     }
-
     return maxScore
 }
